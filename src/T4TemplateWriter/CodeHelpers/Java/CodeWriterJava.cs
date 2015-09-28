46122332a35c8ba0bf0ba72172283053bc9ea8d4
Anahi Hojnadel <anahi.hojnadel@gmail.com>
2015-09-28 17:28:03 -0300

    Minor updates in template

--- src/T4TemplateWriter/CodeHelpers/Java/CodeWriterJava.cs
+++ src/T4TemplateWriter/CodeHelpers/Java/CodeWriterJava.cs
@@ -28,52 +28,5 @@ namespace Vipr.T4TemplateWriter.CodeHelpers.Java
         {
             return "// ";
         }
-
-        public List<SubElementDefinition> GetSubElementDefinitions(Node node)
-        {
-            var parent = node.Parent != null ? node.Parent.Property : null;
-            IEnumerable<SubElementDefinition> list = new List<SubElementDefinition>();
-            foreach (var childProperty in node.ChildProperties)
-            {
-                var subList = GetSubElementDefinitionsRecursively(parent, childProperty.Property, true, new List<SubElementDefinition>());
-                list = list.Union(subList);
-            }
-
-            return list.ToList();
-        }
-
-        private List<SubElementDefinition> GetSubElementDefinitionsRecursively(OdcmProperty parent, OdcmProperty child, bool isNavigation, List<SubElementDefinition> list)
-        {
-            if (child != null && child.IsCollection && parent != null )
-            {
-                var sd = new SubElementDefinition(child.Name, child.Type, parent.Type);
-                if (!list.Contains(sd))
-                {
-                    list.Add(sd);
-                }
-            }
-
-            var clazz = child.Type as OdcmClass;
-            if (clazz != null)
-            {
-                if (clazz.Properties != null)
-                {
-                    var nav = clazz.NavigationProperties().ToList();
-                    foreach (var p in clazz.Properties)
-                    {
-                        var alreadyAdded =
-                            list.Any(se => se.PropertyType.FullName.Equals(p.Type.FullName) && se.ParentType.FullName.Equals(child.Type.FullName));
-
-                        if (!alreadyAdded)
-                        {
-                            var subList = GetSubElementDefinitionsRecursively(child, p, nav.Contains(p), list);
-                            list = list.Union(subList).ToList();
-                        }
-                    }
-                }
-            }
-
-            return list;
-        }
     }
 }
